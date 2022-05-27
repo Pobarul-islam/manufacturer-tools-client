3daa400ae32b2ee16d3970cff34847ec8bb2b101
unknown <Pobarul-islam>
2022-05-27 17:46:12 +0600

    modified

--- src/Login/Signup.js
+++ src/Login/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../Pages/Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
@@ -20,7 +21,9 @@ const SignUp = () => {
 
     let signInError;
 
-
+    if (loading || gLoading || updating) {
+        return <Loading></Loading>
+    }
 
     if (error || gError || updateError) {
         signInError = <p className='text-red-500'><small>{error?.message || gError?.message || updateError?.message}</small></p>
