fad72e1af0f305a69dd582490700bbab9bf127e0
Pobarul-islam <pobarul3@gmail.com>
2022-06-01 11:20:39 +0600

    modified

--- src/Pages/Review/ReviewCard.js
+++ src/Pages/Review/ReviewCard.js
@@ -2,7 +2,7 @@ import { Rating } from '@mui/material';
 import React from 'react'
 const ReviewCard = ({ review }) => {
     return (
-        <div className="card w-20 bg-base-100 pt-5 shadow-2xl">
+        <div className="card bg-base-100 pt-5 shadow-2xl">
             <div className="card-body">
                 <Rating
                     name="simple-controlled"
